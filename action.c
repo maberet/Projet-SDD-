a6b3a000ae46b2aec516d9e714595af9497968f6
malegall4 <malegall4@ada.local.isima.fr>
2022-02-10 11:59:51 +0100

    Fin TP10/02

--- action.c
+++ action.c
@@ -3,5 +3,87 @@
 #include"action.h"
 #include"semaine.h"
 
+/*-----------------------------------------------------
+InitialisationAction : Initialise la liste des actions.
 
+Entrée : Rien.
+
+Sortie : Liste d'actions vide.
+-----------------------------------------------------*/
+
+Listeaction_t InitialisationAction(void){
+    return NULL;
+} 
+
+
+
+/*--------------------------------------------------
+ListeActionVide : Teste si la liste est vide ou non.
+
+Entrée : Liste d'actions.
+
+Sortie : 1 si elle est vide, 0 sinon.
+--------------------------------------------------*/
+
+int ListeActionVide(Listeaction_t listeact){
+    
+    int result = 0;
+
+    if (listeact == NULL){
+        result = 1;
+    }
+
+    return result;
+}
+
+
+
+/*---------------------------------------------------------------------------------
+InsertionEnTeteAction : Insère en tête de liste la structure d'actions en paramètre
+
+Entrée : Structure d'actions act, Liste d'actions listeact.
+
+Sortie : Liste d'actions mise à jour.
+---------------------------------------------------------------------------------*/
+
+Listeaction_t InsertionEnTeteAction(Action_t act, Listeaction_t listeact){
+   
+    MaillonAct_t *m;
+    m = (MaillonAct_t *)malloc(sizeof(MaillonAct_t));
+
+    if (m == NULL){
+        printf("Erreur d'allocation");
+    }
+    else{
+        m->action = act;
+        m->suiv = listeact;
+    }
+
+    return m;
+}
+
+
+
+/*-----------------------------------------------------------------------------
+InsertionAction : Insère la structure d'actions à la bonne place dans la liste.
+
+Entrée : Structure d'actions act, Liste d'actions listeact.
+
+Sortie : Liste d'actions listeact.
+-----------------------------------------------------------------------------*/
+
+Listeaction_t InsertionAction(Action_t act,Listeaction_t listeact){
+    
+    if (ListeActionVide(listeact)){
+        InsertionEnTeteAction(act,listeact);
+    }
+    if ((listeact->action).jourheure > act.jourheure){
+        InsertionEnTeteAction(act,listeact);
+    }
+    else{
+        InsertionAction(act,listeact->suiv);
+    }
+
+    return listeact;
+}
 
--- action.h
+++ action.h
@@ -1,8 +1,14 @@
+typedef struct maillonact{
+    Action_t action;
+    struct maillonact_t *suiv
+} MaillonAct_t,*Listeaction_t; 
+
 typedef struct action {
     char jourheure[3];
     char action[10];
+}Action_t,
 
-    struct action *suiv;
-}Action,*Listeaction; 
-
-
+Listeaction_t InitialisationAction(void);
+int ListeActionVide(Listeaction_t);
+Listeaction_t InsertionEnTeteAction(Action_t, Listeaction_t);
+Listeaction_t InsertionAction(Action_t, Listeaction_t);
