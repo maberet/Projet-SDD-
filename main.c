2f8633be29e1cde9639f98d4b98b04cfbb99ecd5
MLG <mlegall29asb@gmail.com>
2022-02-15 17:29:13 +0100

    nul

--- main.c
+++ main.c
@@ -6,6 +6,7 @@ int main()
     Semaine_t           sem;
 
     FILE              * file;
+
     listesem = InitialisationSemaine();
     file = fopen("action.txt","r");
     if (file == NULL)
@@ -16,6 +17,7 @@ int main()
     {
         while(!feof(file))
         {
+            sem.act = (MaillonAct_t *)malloc(sizeof(MaillonAct_t));
             fscanf(file,"%6s %3s %[^\n]%*c",sem.anneesemaine,siuu.jourheure,siuu.action);
             (sem.act)->action=siuu;
             listesem = InsertionSemaine(sem,listesem);
