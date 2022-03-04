7a0ba203b12cdcfb9932eb6cb61e2692c64dcbd3
MLG <mlegall29asb@gmail.com>
2022-03-04 11:19:01 +0100

    motif

--- main.c
+++ main.c
@@ -1,15 +1,21 @@
-#include "semaine.h"
+#include "motif.h"
 
 int main(int argc, char ** argv)
 {
     Listesem_t listesem;
+    Date_t date[TAILLEMAX];
+
     listesem = InitialisationSemaine();
 
     listesem = InsertionDonnees(argv[1],listesem);
 
-    AfficherListeSemaine(listesem);
+    //AfficherListeSemaine(listesem);
 
     Sauvegarde(argv[2],listesem);
+
+    int i = Motif(argv[3],listesem,date,&date[TAILLEMAX]);
+
+    AfficherListeContigue(argv[3],date,&date[i]);
 
     return 0;
 }
--- motif.h
+++ motif.h
@@ -12,3 +12,6 @@ typedef struct date
 	char jourheure[4];
 	char action[11];
 } Date_t;
+
+int Motif(char * motif, Listesem_t listesem, Date_t date[], Date_t * findate);
+void AfficherListeContigue(char * motif, Date_t date[], Date_t * findate);
