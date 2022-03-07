8ddf2b0a9ba9aec9f00e240ad59a026b1b9bb54a
MLG <mlegall29asb@gmail.com>
2022-03-07 12:36:12 +0100

    modification du main

--- main.c
+++ main.c
@@ -3,19 +3,35 @@
 int main(int argc, char ** argv)
 {
     Listesem_t listesem;
-    Date_t date[TAILLEMAX];
+    Date_t     date[TAILLEMAX];
+    int        i;
+    int        x;
+    char     * sauv;
+    char     * motif;
 
     listesem = InitialisationSemaine();
 
     listesem = InsertionDonnees(argv[1],listesem);
 
-    //AfficherListeSemaine(listesem);
+    printf("Choisissez une action : \n");
+    printf("1 - Afficher la liste des semaines\n2 - Sauvegarder dans un fichier\n3 - Afficher la liste des semaines contenant un motif\n4 - Supprimer une action\n");
+    scanf("%d", &x);
 
-    Sauvegarde(argv[2],listesem);
+    switch(x)
+    {
+        case 1: AfficherListeSemaine(listesem); break;
 
-    int i = Motif(argv[3],listesem,date,&date[TAILLEMAX]);
+        case 2: printf("Nom du fichier de sauvegarde?\n");
+                scanf("%s",sauv);
+                Sauvegarde(sauv,listesem); break;
 
-    AfficherListeContigue(argv[3],date,&date[i]);
+        case 3: printf("Motif?\n");
+                scanf("%s",motif);
+                i = Motif(motif,listesem,date,&date[TAILLEMAX]);
+                AfficherListeContigue(motif,date,&date[i]); break;
+        
+        case 4: break;
+    }
 
     return 0;
 }
