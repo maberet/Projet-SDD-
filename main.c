db673aaaa1f8691a83d5a5cc6f2eb90b027c2d29
MLG <mlegall29asb@gmail.com>
2022-03-10 10:31:41 +0100

    Mauvais charactère

--- main.c
+++ main.c
@@ -19,42 +19,62 @@ int main(int argc, char ** argv)
 
     listesem = InsertionDonnees(argv[1],listesem);
 
+    printf("Choisissez une action : \n");
+    printf("1 - Afficher la liste des semaines\n2 - Sauvegarder dans un fichier\n3 - Afficher la liste des semaines contenant un motif\n4 - Supprimer une action\n5 - Sortie\n");
+
     while(!sortie)
     {
+        while(scanf("%d", &x) == 0)
+        {
+            printf("Mauvais charactère\n");
+
+            printf("Choisissez une action : \n");
+            printf("1 - Afficher la liste des semaines\n2 - Sauvegarder dans un fichier\n3 - Afficher la liste des semaines contenant un motif\n4 - Supprimer une action\n5 - Sortie\n");
+            scanf("%d", &x);
+            getchar();
+        }
+
+        if(x>0 && x<6)
+        {
+            switch(x)
+            {
+                case 1: AfficherListeSemaine(listesem); break;
+
+                case 2: printf("Nom du fichier de sauvegarde?\n");
+                        scanf("%s",sauv);
+                        Sauvegarde(sauv,listesem); break;
+
+                case 3: printf("Motif?\n");
+                        scanf("%s",motif);
+                        i = Motif(motif,listesem,date,&date[TAILLEMAX]);
+                        AfficherListeContigue(motif,date,&date[i]); break;
+            
+                case 4: printf("Annee?\n");
+                        scanf("%s",annee);
+                        printf("Semaine?\n");
+                        scanf("%s",semaine);
+                        printf("Jour?\n");
+                        scanf("%d",&jour);
+                        printf("Heure?\n");
+                        scanf("%s",heure);
+                        b = SuppressionAction(listesem, annee, semaine, jour, heure);
+                        if (b)
+                        {
+                            listesem=SuppressionMaillonSemaine(listesem, annee, semaine);
+                        } 
+                        break;
+
+                case 5: sortie = vrai; break;
+            }
+        }
+        else
+        {
+            printf("Mauvais charactère\n");
+        }
+
         printf("Choisissez une action : \n");
         printf("1 - Afficher la liste des semaines\n2 - Sauvegarder dans un fichier\n3 - Afficher la liste des semaines contenant un motif\n4 - Supprimer une action\n5 - Sortie\n");
-        scanf("%d", &x);
-
-        switch(x)
-        {
-            case 1: AfficherListeSemaine(listesem); break;
-
-            case 2: printf("Nom du fichier de sauvegarde?\n");
-                    scanf("%s",sauv);
-                    Sauvegarde(sauv,listesem); break;
-
-            case 3: printf("Motif?\n");
-                    scanf("%s",motif);
-                    i = Motif(motif,listesem,date,&date[TAILLEMAX]);
-                    AfficherListeContigue(motif,date,&date[i]); break;
-        
-            case 4: printf("Annee?\n");
-                    scanf("%s",annee);
-                    printf("Semaine?\n");
-                    scanf("%s",semaine);
-                    printf("Jour?\n");
-                    scanf("%d",&jour);
-                    printf("Heure?\n");
-                    scanf("%s",heure);
-                    b = SuppressionAction(listesem, annee, semaine, jour, heure);
-                    if (b)
-                    {
-                        listesem=SuppressionMaillonSemaine(listesem, annee, semaine);
-                    } 
-                    break;
-
-            case 5: sortie = vrai; break;
-        }
+    
     }
 
     LiberationSemaines(listesem);
